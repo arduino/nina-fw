ad851ea424b9446f71f3136855e8bf34b6fcbad7
Alexander Entinger <cto@lxrobotics.com>
2020-09-02 08:49:32 +0200

    Add Wire to the libraries which need to be compiled

--- arduino/component.mk
+++ arduino/component.mk
@@ -1,3 +1,3 @@
-COMPONENT_ADD_INCLUDEDIRS := cores/esp32 libraries/SPIS/src libraries/WiFi/src libraries/ArduinoECCX08/src
+COMPONENT_ADD_INCLUDEDIRS := cores/esp32 libraries/SPIS/src libraries/WiFi/src libraries/Wire/src libraries/ArduinoECCX08/src
 
-COMPONENT_SRCDIRS := cores/esp32 libraries/SPIS/src libraries/WiFi/src libraries/ArduinoECCX08/src
+COMPONENT_SRCDIRS := cores/esp32 libraries/SPIS/src libraries/WiFi/src libraries/Wire/src libraries/ArduinoECCX08/src
