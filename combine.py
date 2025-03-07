53405a2f07d62346c95ac86836f0e1a1c6faeca6
pennam <m.pennasilico@arduino.cc>
2025-03-07 12:30:18 +0100

    partitions: update combine script

--- combine.py
+++ combine.py
@@ -8,6 +8,15 @@ phyData = open("data/phy.bin", "rb").read()
 certsData = open("data/roots.pem", "rb").read()
 appData = open("build/nina-fw.bin", "rb").read()
 
+# Offset   Size     Name
+# 0x001000 0x007000 bootloader
+# 0x008000 0x001000 partitions
+# 0x009000 0x001000 phy data
+# 0x00A000 0x026000 certs
+# 0x030000 0x180000 app
+# 0x1B0000 0x040000 spiffs
+# 0x1F0000 0x010000 nvs
+
 # calculate the output binary size, app offset 
 outputSize = 0x30000 + len(appData)
 if (outputSize % 1024):
@@ -24,13 +33,13 @@ for i in range(0, len(partitionData)):
 	outputData[0x8000 + i] = partitionData[i]
 
 for i in range(0, len(phyData)):
-        outputData[0xf000 + i] = phyData[i]
+    outputData[0x9000 + i] = phyData[i]
 
 for i in range(0, len(certsData)):
-        outputData[0x10000 + i] = certsData[i]
+    outputData[0xA000 + i] = certsData[i]
 
 # zero terminate the pem file
-outputData[0x10000 + len(certsData)] = 0
+outputData[0xA000 + len(certsData)] = 0
 
 for i in range(0, len(appData)):
 	outputData[0x30000 + i] = appData[i]
